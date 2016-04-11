038918762a59cbd8375889ebc2ef4a2e937439bc
Ananth Ravi Kumar <ananthravikumar@berkeley.edu>
2016-04-11 12:10:41 -0700

    generalized

--- evenflow/getcountmat_9.py
+++ evenflow/getcountmat_9.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-def getcountmat(tuplemat, nbinmat, sX, sY, nodatacode):
+
+def getcountmat(tuplemat, nbinmat, sx, sy, nodatacode):
     """
 
     :param tuplemat: matrix to be processed.
     :param nbinmat: matrix of bin edges
-    :param sX: scalar
-    :param sY: scalar
+    :param sx: scalar
+    :param sy: scalar
     :param nodatacode: indicates field has no data value
     :return: number of valid entries (ncount) and matrix with 1s where there
     are valid entries, and 0 elsewhere.
@@ -16,31 +17,36 @@ def getcountmat(tuplemat, nbinmat, sX, sY, nodatacode):
     1) values of dimXt, dimYw and dimYf are hard-coded and reduced by 1
     to solve indexing issues. Matlab indexes from 1, python indexes from 0.
     2) likewise is done when assigning dimXt, dimYw, dimYf to tuplemat[i,j].
-    3) Generalized to an n x n matrix. Might cause compatibility issues
+    3) Generalized to an n x n matrix. Might cause compatibility issues with
+    getShannonBits.
     """
-    #Todo: vectorize as much as possible.
-    #Todo: generalize to arbitrary sized tuplemat/return matrix.
+    # Todo: vectorize as much as possible.
     # catching faulty parameters.
-    assert sX >= 1
-    assert sY >= 1
-    assert sX-1 < len(nbinmat)
-    assert sY-1 < len(nbinmat)
+    assert sx >= 1
+    assert sy >= 1
+    assert sx-1 < len(nbinmat)
+    assert sy-1 < len(nbinmat)
     assert np.size(tuplemat) > 0
     assert np.size(nbinmat) > 0
-    tupshape = np.shape(tuplemat)
-    assert tupshape[1] == 3
 
-    ndata, _ = np.shape(tuplemat)
+    # function begins
+    rownum, colnum = np.shape(tuplemat)
     binmatarr = np.asarray(nbinmat)
-    c = np.zeros(shape=(binmatarr[sX-1], binmatarr[sY-1], binmatarr[sY-1]))
+    dimlist = []
+    # constructing matrix of required proportions
+    for i in range(colnum):
+        if i == 0:
+            dimlist.append(binmatarr[sx - 1])
+        else:
+            dimlist.append(binmatarr[sy - 1])
+    c = np.zeros(shape=tuple(dimlist))
     ncounts = 0
-    for i in range(ndata):
-        dimXt = tuplemat[i, 0]
-        dimYw = tuplemat[i, 1]
-        dimYf = tuplemat[i, 2]
-        if dimXt != nodatacode and dimYw != nodatacode and dimYf != nodatacode:
-            c[dimXt-1, dimYw-1, dimYf-1] += 1
+    for i in range(rownum):
+        row = tuplemat[i, :]
+        row[row == nodatacode] = np.nan
+        if np.isfinite(row).all():
+            newrow = tuple(row.astype(int) - 1)
+            c[newrow] += 1
             ncounts += 1
     return c, ncounts
-
 
--- evenflow/tests/test_getcountmat.py
+++ evenflow/tests/test_getcountmat.py
@@ -4,21 +4,26 @@ import os
 
 path = os.path.dirname(os.path.realpath(__file__))
 
+
 def test_basic():
-    nbinmat = np.array([11,11,11]).reshape(3,1)
-    tuplemat = np.genfromtxt(path + '\mat.txt',delimiter=',')
+    nbinmat = np.array([11, 11, 11]).reshape(3, 1)
+    tuplemat = np.genfromtxt(path + '\mat.txt', delimiter=',')
     nodatacode = -9999
-    sX, sY = 3,3
+    sx, sy = 3, 3
     result = np.genfromtxt(path + '\getcountresult.txt', delimiter=',')
-    result.reshape((11,11,11))
+    result.reshape((11, 11, 11))
     for matrix in result:
         matrix = np.transpose(matrix)
-    final, count = gcm.getcountmat(tuplemat, nbinmat, sX, sY, nodatacode)
+    final, count = gcm.getcountmat(tuplemat, nbinmat, sx, sy, nodatacode)
     assert final.all() == result.all()
 
+
 def test_assignment():
-    nbinmat = np.array([3,3,3]).reshape(3,1)
-    tuplemat = np.zeros(shape=(4,4))
+    nbinmat = np.array([3, 3, 3]).reshape(3, 1)
+    tuplemat = np.zeros(shape=(4, 4))
+
+    return 0
+
 
 def test_indices():
     return 0
