a968ad2d22a96956435770c9bd0c77ae6592381c
ace_siming_hao <ace.hao@berkeley.edu>
2016-04-17 16:18:50 -0700

    added path to pass travis.

--- infoflow/tests/test_quantize.py
+++ infoflow/tests/test_quantize.py
@@ -2,13 +2,14 @@ import sys
 import os
 import numpy as np
 from infoflow import quantize as qtz
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ''))
+print(sys.path[0])
 
 in_path = 'in_quantize/'
 out_path = 'out_quantize/'
 
-x = np.genfromtxt(in_path+'x.csv', delimiter=',').reshape((1, 202))
-partition = np.genfromtxt(in_path+'partition.csv', delimiter=',').tolist()
-xquant = np.genfromtxt(out_path+'xquant.csv', delimiter=',').reshape((1, 202))
+x = np.genfromtxt(sys.path[0]+in_path+'x.csv', delimiter=',').reshape((1, 202))
+partition = np.genfromtxt(sys.path[0]+ in_path+'partition.csv', delimiter=',').tolist()
+xquant = np.genfromtxt(sys.path[0]+out_path+'xquant.csv', delimiter=',').reshape((1, 202))
 
 assert np.all((xquant == qtz.quantize(x, partition, 0)))
