086a89b1571dd342d82762e8e774074c4d4d2f1d
arkwave <ananthravikumar@gmail.com>
2016-04-21 00:56:36 -0800

    finished debugging multidimkde

--- infoflow/multidimkde.py
+++ infoflow/multidimkde.py
@@ -4,16 +4,21 @@ from math import pi, exp, sqrt
 
 def multidimensionalkde(x, xi, bw_coeff):
     """
-    :param x: data matrix, rows are observations and columns are dimensions.
-    :param xi: coordinates at which joint probabilities are to be estimated.
-    :param bw_coeff:
+    :param x: np.ndarray, rows are observations and columns are dimensions.
+    :param xi: np.ndarray, coordinates at which joint probabilities are to be estimated.
+    :param bw_coeff: scalar multiplier that adjusts the rule of thumb bandwidth
     :return: joint probability at xi
     """
+
+    # assertions to ensure input is in right format.
+    assert isinstance(x, np.ndarray)
+    assert isinstance(xi, np.ndarray)
+
     n, d = np.shape(x)
     # pick the bandwidth for each dimension
     h = np.zeros(shape=(1, d))
     for i in range(d):
-        h[0, i] = float(bw_coeff * 1.06 * n**(-1/5) *np.std(x[:, i]))
+        h[0, i] = float(bw_coeff * 1.06 * n**(-1/5) *np.std(x[:, i],ddof=1))
     # estimating probability at xi
     p = np.zeros(shape=(np.shape(xi)[0], 1))
     for k in range(len(p)):
--- infoflow/tests/test_multidimensionalkde.py
+++ infoflow/tests/test_multidimensionalkde.py
@@ -5,21 +5,14 @@ import os
 path = os.path.dirname(os.path.realpath(__file__))
 
 def test_basic():
+    """
+    Note about test: ans[0][0] - result ~ 10^-34. If system is chaotic, could cause errors. Double check
+    floating point bs.
+    """
     x = np.genfromtxt(path + '\mdkde_input.txt', delimiter='\t')
     xi = np.array([6.921252431034000, -18.53087722427270, 37.295725015630570])
     bw_coeff = 1
-    result = 4.67184882019107e-22
-    ans = multkde.multidimensionalkde(x, xi, bw_coeff)
-    assert np.isclose(ans, result).all()
-
-def test_inputs():
-    return 0
-
-
-def test_outputs():
-    return 0
-
-
-def test_all():
-    return 0
+    result = 4.67184882019e-22
+    ans = multkde.multidimensionalkde(x, xi, bw_coeff).reshape(1,3)
+    assert np.isclose(ans[0,0], result)
 
